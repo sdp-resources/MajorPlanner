a69c42e168c188c9dec2c78cb7313c639dce3319
Haris Skiadas <cskiadas@gmail.com>
2019-10-04 14:31:12 -0400

    Add integration and unit test configurations

---
+++ src/test/java/sampleNormalTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class sampleNormalTest {
+  @Test
+  public void aTest() {
+    assertEquals(true, true);
+    assertTrue(true);
+    assertFalse(false);
+  }
+}
