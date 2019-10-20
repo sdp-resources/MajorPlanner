6e3403d4e6e58669669f031d8cd26a65c573fa6d
Benjamin Givens <btg99email@gmail.com>
2019-10-20 17:34:20 -0400

    Add scenarios to CreateSchedule acceptance test

--- src/test/java/acceptance/CreateScheduleSteps.java
+++ src/test/java/acceptance/CreateScheduleSteps.java
@@ -18,6 +18,7 @@ public class CreateScheduleSteps {
     private Controller controller;
     private Session session;
     private Gateway gateway;
+    private Response response;
 
     @Given("{word} is a {word} and logged in")
     public void HasRoleAndLoggedIn(String username, String role) {
@@ -28,8 +29,22 @@ public class CreateScheduleSteps {
 
     @When("{word} creates a schedule called {string}")
     public void CreateSchedule(String username, String scheduleName) {
-        Response response = controller.createSchedule(session, username, scheduleName);
+        response = controller.createSchedule(session, username, scheduleName);
+    }
+
+    @When("they create a schedule called {string} in the name of {word}")
+    public void CreatesAScheduleInTheNameOf(String sessionUser, String scheduleName, String username) {
+        response = controller.createSchedule(session, username, scheduleName);
+    }
+
+    @Then("the response has no error")
+    public void theResponseHasNoError() {
         Assert.assertFalse(response.containsError());
+    }
+
+    @Then("the response has an error")
+    public void theResponseHasAnError() {
+        Assert.assertTrue(response.containsError());
     }
 
     @Then("there exists a schedule called {string} owned by {word}")
--- src/test/resources/acceptance/create_schedule.feature
+++ src/test/resources/acceptance/create_schedule.feature
@@ -2,4 +2,16 @@ Feature: Create Blank Schedule
   Scenario: User creates schedule
     Given Ben is a user and logged in
     When Ben creates a schedule called "My Schedule"
-    Then there exists a schedule called "My Schedule" owned by Ben
+    Then the response has no error
+    And there exists a schedule called "My Schedule" owned by Ben
+
+  Scenario: Admin creates a schedule
+    Given MrAdmin is a admin and logged in
+    When they create a schedule called "Schedule for Haris" in the name of Haris
+    Then the response has no error
+    And there exists a schedule called "Schedule for Haris" owned by Haris
+
+  Scenario: User creates a schedule for someone else
+    Given Mackenzie is a user and logged in
+    When they create a schedule called "Schedule for someone else" in the name of Brian
+    Then the response has an error
