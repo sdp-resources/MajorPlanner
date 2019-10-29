518454b204664bc73ac1f83bafaaa467f90a137a
Benjamin Givens <btg99email@gmail.com>
2019-10-29 12:08:55 -0400

    Refactor GatewayBackedAuthorizer

--- src/test/java/majorPlanner/GatewayBackedAuthorizerTest.java
+++ src/test/java/majorPlanner/GatewayBackedAuthorizerTest.java
@@ -15,29 +15,27 @@ public class GatewayBackedAuthorizerTest {
     @Test
     public void userInSessionMatchesOwner()
     {
-        CreateScheduleRequest request = new CreateScheduleRequest("dantgeo");
-        request.setSession(new Session(null, "dantgeo", Role.User));
-        Authorizer authorizer = new GatewayBackedAuthorizer();
-        Response response = authorizer.authorize(request);
+        Response response = authorize("dantgeo", "dantgeo", Role.User);
         Assert.assertFalse(response.containsError());
     }
 
     @Test
     public void userInSessionDoesNotMatchOwner(){
-        CreateScheduleRequest request = new CreateScheduleRequest("dantgeo");
-        request.setSession(new Session(null, "marsht", Role.User));
-        Authorizer authorizer = new GatewayBackedAuthorizer();
-        Response response = authorizer.authorize(request);
+        Response response = authorize("dantgeo", "marsht", Role.User);
         Assert.assertTrue(response.containsError());
         Assert.assertEquals(GatewayBackedAuthorizer.USER_MISMATCH_MESSAGE, ((ErrorResponse)response).getError());
     }
 
     @Test
     public void adminInSessionIgnoresRequirements(){
-        CreateScheduleRequest request = new CreateScheduleRequest("dantgeo");
-        request.setSession(new Session(null, "marsht", Role.Admin));
-        Authorizer authorizer = new GatewayBackedAuthorizer();
-        Response response = authorizer.authorize(request);
+        Response response = authorize("dantgeo", "marsht", Role.Admin);
         Assert.assertFalse(response.containsError());
+    }
+
+    private Response authorize(String scheduleOwner, String sessionUser, Role role) {
+        CreateScheduleRequest request = new CreateScheduleRequest(scheduleOwner);
+        request.setSession(new Session(null, sessionUser, role));
+        Authorizer authorizer = new GatewayBackedAuthorizer();
+        return authorizer.authorize(request);
     }
 }
